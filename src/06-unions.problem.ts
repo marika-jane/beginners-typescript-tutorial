6e8d185d01ac267c78b9fa326150594a16b87f8b
Marika <66046690+marika-jane@users.noreply.github.com>
2022-10-19 10:32:36 +1300

    solution 6 union type

--- src/06-unions.problem.ts
+++ src/06-unions.problem.ts
@@ -8,7 +8,7 @@ interface User {
    * - 'user'
    * - 'super-admin'
    */
-  role: string;
+  role: "admin" | "user" | "super-admin";  // called a union type
 }
 
 export const defaultUser: User = {
