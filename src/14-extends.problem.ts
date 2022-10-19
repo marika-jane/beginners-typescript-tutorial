b59bc2e059b8fae74746ae0e6158fd5cff7cf238
Marika <66046690+marika-jane@users.noreply.github.com>
2022-10-20 12:05:08 +1300

    extends solution 14

--- src/14-extends.problem.ts
+++ src/14-extends.problem.ts
@@ -6,20 +6,33 @@ import { Equal, Expect } from "./helpers/type-utils";
  * make it more DRY?
  */
 
-interface User {
-  id: string;
+// interface SharedInterface {
+//   id: string;
+//   firstName: string;
+//   lastName: string;
+//   title: string;
+//   body: string;
+//   comment: string;
+// }
+
+// type User = Omit<SharedInterface, "title" | "body" | "comment">
+// type Post = Omit<SharedInterface, "firstName" | "lastName" | "comment">
+// type Comment = Omit<SharedInterface, "title" | "body" | "firstName" | "lastName">
+
+interface SharedProperty {
+  id: string
+}
+
+interface User extends SharedProperty {
   firstName: string;
   lastName: string;
 }
-
-interface Post {
-  id: string;
+interface Post extends SharedProperty {
   title: string;
   body: string;
 }
 
-interface Comment {
-  id: string;
+interface Comment extends SharedProperty {
   comment: string;
 }
 
