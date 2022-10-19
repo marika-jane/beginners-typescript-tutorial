718f58d739699dc28d1b413f0466701d61c97d94
Marika <66046690+marika-jane@users.noreply.github.com>
2022-10-20 11:55:59 +1300

    13 use instanceof

--- src/13-catch-blocks.problem.ts
+++ src/13-catch-blocks.problem.ts
@@ -6,7 +6,10 @@ const tryCatchDemo = (state: "fail" | "succeed") => {
       throw new Error("Failure!");
     }
   } catch (e) {
-    return e.message;
+    if (e instanceof Error) {
+      return e.message;
+    }
+    // else??? 
   }
 };
 
